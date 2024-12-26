196436a84ab466a9606577658917615e3c144749
De Lin <qsf44@outlook.com>
2024-12-26 16:20:32 +0800

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   package-lock.json
    	modified:   package.json
    	new file:   src/controllers/userControllers.ts
    	modified:   src/routes/authRoutes.ts
    	modified:   src/server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,15 @@
 import express from "express";
-
+import authRoutes from "./routes/authRoutes";
+import cors from "cors";
+import morgan from "morgan";
+import { PORT } from "./config/env";
 const app = express();
-const port = 3000;
+const port = PORT ;
 
+app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
+app.use("/auth", authRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
