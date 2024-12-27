08fa3f8fed74fc38f76df6b03099d88b724786ef
De Lin <qsf44@outlook.com>
2024-12-27 17:55:22 +0800

    implement user registration functionality and update user model

---
+++ src/controllers/authController.ts
@@ -0,0 +1,42 @@
+import { Request, Response } from "express";
+import bcrypt from "bcrypt";
+import User from "../models/userModel";
+import { MongoError } from "mongodb";
+// POST /auth/register
+export const register = async (req: Request, res: Response) => {
+  try {
+    const { email, password } = req.body;
+    // Check if email and password are provided
+    //if not return a 400 error
+    if (!email || !password) {
+      res.status(400).json({ message: "Please provide email and password" });
+      return;
+    }
+    //check if email is a valid email
+    //if not return a 400 error
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      res.status(400).json({ message: "Please provide a valid email" });
+      return;
+    }
+    const hashedPassword = await bcrypt.hash(password, 12);
+    const newUser = await User.create({ email, password: hashedPassword });
+    res.status(201).json(newUser);
+    return;
+  } catch (err) {
+    // Check if the error is a validation error
+    if (err instanceof Error && err.name === "ValidationError") {
+      res.status(400).json(err);
+      return;
+    }
+    // Check if the error is a duplicate key error
+    if (err instanceof MongoError && err.code === 11000) {
+      // This is a duplicate key error
+      res.status(400).json(err);
+      return;
+    }
+    // If the error is not a validation error or a duplicate key error, return a 500 error
+    res.status(500).json(err);
+    return;
+  }
+};
--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,15 +1,11 @@
-import express from "express"
-import {getAllUsers,createUser} from "../controllers/userControllers"
-const router = express.Router()
+import express from "express";
+import { register } from "../controllers/authController";
+const router = express.Router();
 
-router.post("/register", (req, res) => {
-  res.send("Register route")
-})
+router.post("/register", register);
 
 router.post("/login", (req, res) => {
-    res.send("Login route")
-})
+  res.send("Login route");
+});
 
-router.post("/", createUser)
-router.get("/", getAllUsers)
-export default router
+export default router;
