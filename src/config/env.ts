7c591333cf4f569019e7cf6bac1d14622bbbc4bb
De Lin <qsf44@outlook.com>
2024-12-30 17:55:08 +0800

    added 2 jwt secret

--- src/config/env.ts
+++ src/config/env.ts
@@ -3,5 +3,6 @@ import dotenv from "dotenv"
 dotenv.config()
 
 export const PORT = process.env.PORT || "8000"
-export const JWT_SECRET = process.env.JWT_SECRET || "secret"
 export const MONGO_URI = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/DevQnA"
+export const JWT_REFRESH_SECRET = process.env.JWT_REFRESH_SECRET || "refresh_secret"
+export const JWT_ACCESS_SECRET = process.env.JWT_ACCESS_SECRET || "access_secret"
