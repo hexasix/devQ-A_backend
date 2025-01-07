1b66b0fa3b95488473502986efa93166aeb236b6
De Lin <qsf44@outlook.com>
2025-01-07 15:55:36 +0800

    eslint

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,8 +1,11 @@
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
-export const PORT = process.env.PORT || "8000"
-export const MONGO_URI = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/DevQnA"
-export const JWT_REFRESH_SECRET = process.env.JWT_REFRESH_SECRET || "refresh_secret"
-export const JWT_ACCESS_SECRET = process.env.JWT_ACCESS_SECRET || "access_secret"
+export const PORT = process.env.PORT || "8000";
+export const MONGO_URI =
+  process.env.MONGO_URI || "mongodb://127.0.0.1:27017/DevQnA";
+export const JWT_REFRESH_SECRET =
+  process.env.JWT_REFRESH_SECRET || "refresh_secret";
+export const JWT_ACCESS_SECRET =
+  process.env.JWT_ACCESS_SECRET || "access_secret";
